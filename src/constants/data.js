9e27804806c8c2770330c4f9410a6b126c0bfdc1
sury <surysanchezsales@gmail.com>
2024-02-17 19:22:51 -0500

    update style

--- src/constants/data.js
+++ src/constants/data.js
@@ -41,40 +41,40 @@ const OtherCustomWorks = [
 
 const reviewItems = [
   {
-  name: 'Alberto Delgado',
+  name: 'Alicia',
   title: '¡Muy Recomendado!',
   description: 'Adrian hizo un trabajo increible con mi casa. Instalo todos los cabinetes nuevo de la cocina y quedaron espectaculares. Ahora esta remodelando mi patio, instalando una nueva cerca.',
-  img: '{image.portrait2}',
+  img: `${images.portrait2}`,
   },
   {
   name: 'Rene Garcia',
   title: 'Buena Comunicación y Servicio Oportuno',
   description: 'Adrian instaló molduras en mi sala con una precisión increíble. Se nota su experiencia. Definitivamente lo contrataré de nuevo.',
-  img: '',
+  img: `${images.man4}`,
   },
   {
-  name: 'Alicia',
+  name: 'Alberto Delgado',
   title: ' Trabajo Increíble',
   description: 'Adrian hizo unas estanterías increíbles para mi sala. Muy profesional y detallista. ¡Lo recomiendo mucho!',
-  img: '{images.man1}',
+  img: ''
   },
   {
   name: 'Carlos',
   title: ' Clear Communication, Timely Service',
   description: 'Working with Adrian on my home upgrade was smooth. Clear communication and on-time delivery, with just a few minor hiccups. Overall, a great job!',
-  img: '{images.portrait3}',
+  img: `${images.portrait3}`,
   },
   {
   name: 'Sophia',
   title: 'Experto y Preciso',
   description: 'Fue genial en mi proyecto de renovación. Buena comunicación y todo a tiempo. La calidad superó mis expectativas.',
-  img: '{images.man2}',
+  img: `${images.man2}`,
   },
   {
   name: 'Martha',
   title: 'Highly Recommended Craftsperson!',
   description: 'He transformed my living room with new floors. The quality of his work and unique design exceeded expectations. For a skilled carpenter, Adrian is the go-to choice.',
-  img: '{images.portrait4}',
+  img: `${images.portrait4}`,
   },
 ]
 
--- src/container/ReviewDetails/ReviewDetails.jsx
+++ src/container/ReviewDetails/ReviewDetails.jsx
@@ -8,9 +8,9 @@ function ReviewDetails({reviewDetail}) {
            
             <div className="shadow-effect">
                 {/* Ternary: {score > 90 ? "A" : "B or less"}</li> */}
-            {img === '' ? <i class="fa-solid fa-image-portrait fa-2xl" ></i> : <img className="img-circle" src={img}  />}
+            {img === '' ? <i class="fa-solid fa-image-portrait fa-2xl mb-4" ></i> : <img className="img-circle" src={img}  />}
 
-                <h4 style={{color: 'orangered'}} className='p__opensans'>{title}</h4>
+                <h4 style={{color: 'orangered'}} className='p__opensans title'>{title}</h4>
                 <p>{description}</p>
             </div>
             <div className="review-name">
